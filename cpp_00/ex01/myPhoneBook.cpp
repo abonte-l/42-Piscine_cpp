5d59de888b87ca864bdd7cff1690b1829d8a7904
abonte-l <66364998+abonte-l@users.noreply.github.com>
2022-05-27 18:11:38 +0200

    Add files via upload

--- cpp_00/ex01/myPhoneBook.cpp
+++ cpp_00/ex01/myPhoneBook.cpp
@@ -1,12 +1,22 @@
 #include "PhoneBook.hpp"
 
+
 int	main() {
 
 	PhoneBook myPhoneBook;
 	homeDisplay();
 	int command = 0;
+	std::string commandStr;
 	do {
 		command = commandCollect();
 	} while (command == 0);
-			
+	if (command == 1) {
+
+	}
+	else if (command == 2) {
+
+	}
+	else if (command == 3) {
+		exit(0);
+	}
 }
