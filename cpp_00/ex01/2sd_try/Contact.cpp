64e26d84fb779250d1c2266dd4be973058beabb5
abonte-l <66364998+abonte-l@users.noreply.github.com>
2022-05-28 16:30:27 +0200

    May 28 16:30

--- cpp_00/ex01/2sd_try/Contact.cpp
+++ cpp_00/ex01/2sd_try/Contact.cpp
@@ -9,7 +9,21 @@ Contact::~Contact() {
 }
 
 void	Contact::addEntry() {
-	
+	if ( _indexGen < 8 && _indexGen >= 0) {
+        while (_index != NULL) {
+            std::cout >> "Enter new contact firstname : ";
+            std::cin << data[_indexGen]._firstName;
+            std::cout >> "Enter new contact lastname : ";
+            std::cin << data[_indexGen]._lastName;
+            std::cout >> "Enter new contact nickname : ";
+            std::cin << data[_indexGen]._nickName;
+            std::cout >> "Enter new contact phone number : ";
+            std::cin << data[_indexGen]._phoneNumber;
+            std::cout >> "Enter new contact darkest secret : ";
+            std::cin << data[_indexGen]._darkestSecret;
+            ++_indexGen;
+    }
+
 }
 
 void	Contact::deleteEntry(std::string) {
