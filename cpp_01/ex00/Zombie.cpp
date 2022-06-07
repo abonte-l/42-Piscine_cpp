62ec201ebd773b8ce5189f02dc782269e495bf52
abonte-l <abonte-l@student.42.fr>
2022-06-07 13:49:05 +0200

    June 07 13:48

--- cpp_01/ex00/Zombie.cpp
+++ cpp_01/ex00/Zombie.cpp
@@ -10,11 +10,11 @@ Zombie::Zombie(std::string name) : _name(name) {
 };
 
 Zombie::~Zombie() {
-	std::cout << "🧟‍♀️" << _name << " Dying zombie sounds " << "🧟‍♀️" << std::endl;
+	std::cout << "🧟 " << _name  <<  " Dying zombie sounds " << std::endl;
 }
 
 void Zombie::announce(void) {
 
-	std::cout << "🧟‍♀️" << _name << " BraiiiiiiinnnzzzZ... " << "🧟‍♀️" << std::endl;
+	std::cout << "🧟 " <<  _name << " 🧠" << " BraiiiiiiinnnzzzZ... " << "🧠" << std::endl;
 }
 
--- cpp_01/ex00/main.cpp
+++ cpp_01/ex00/main.cpp
@@ -1,14 +1,14 @@
 #include "Zombie.hpp"
 
 const std::string zombiesRandNAme[] {
-	“Crawly McBrains”,
-	“Walker Spew”,
-	“Spewy Eyeball”,
-	“Fester McBrains”,
-	“Fleshy Necro”,
-	“Hungry Guts”,
-	“Brainy Undead”,
-	“Fleshy Fleshington”,
+	"\033[1;32mCrawly McBrains\033[0;1m",
+	"\033[1;32mWalker Spew\033[0;1m",
+	"\033[1;32mSpewy Eyeball\033[0;1m",
+	"\033[1;32mFester McBrains\033[0;1m",
+	"\033[1;32mFleshy Necro\033[0;1m",
+	"\033[1;32mHungry Guts\033[0;1m",
+	"\033[1;32mBrainy Undead\033[0;1m",
+	"\033[1;32mFleshy Fleshington\033[0;1m",
 };
 
 const int zombieRandNameSize = (sizeof(zombiesRandNAme) / sizeof(std::string));
