1683fce5b4fc433c65c1f1fab17870bbc9b200d7
abonte-l <abonte-l@student.42.fr>
2022-06-07 21:26:44 +0200

    June 07 21:26

---
+++ cpp_01/ex04/main.cpp
@@ -0,0 +1,8 @@
+#include "ReplaceIt.hpp"
+
+int		main(int ac, char **av) {
+	//recuperer nom de fichier l'ouvrir avec std::fstream fs 
+	// fs.open 
+	//pour nouveau fichier apparemment open out
+
+}
